3694bd5e191a06c0c5bd085f6ae4edf92d5f106f
Alan <devaliant94@gmail.com>
2020-08-24 13:03:00 -0300

    State presupuesto

--- presupuesto/src/components/Pregunta.js
+++ presupuesto/src/components/Pregunta.js
@@ -1,6 +1,26 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 
 const Pregunta = () => {
+
+  //State para el presupuesto
+  const [cantidad, guardarCantidad] = useState(0);
+
+  // Funcion que lee el presupuesto
+  const definirPresupuesto = e => {
+    console.log( parseInt( e.target.value , 10) );
+  }
+
+  // Submit para definir el presupuesto
+  const agregarPresupuesto = e => {
+    e.preventDefault();
+
+    //Validar
+
+    //En caso de que pase la validación
+  }
+
+
+
   return (
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
@@ -10,6 +30,7 @@ const Pregunta = () => {
           type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={definirPresupuesto}
         />
 
         <input
@@ -19,7 +40,7 @@ const Pregunta = () => {
         />
       </form>
     </Fragment>
-    
+
   );
 }
 
