f710be273ea76a8457d1101668e1f0b8356ec06b
Alan <devaliant94@gmail.com>
2020-08-24 17:17:41 -0300

    state para error en caso de ingresar un numero invalido

--- presupuesto/src/components/Pregunta.js
+++ presupuesto/src/components/Pregunta.js
@@ -5,9 +5,12 @@ const Pregunta = () => {
   //State para el presupuesto
   const [cantidad, guardarCantidad] = useState(0);
 
+  //State para el error
+  const [error, guardarError] = useState(false);
+
   // Funcion que lee el presupuesto
   const definirPresupuesto = e => {
-    console.log( parseInt( e.target.value , 10) );
+    guardarCantidad( parseInt( e.target.value , 10) );
   }
 
   // Submit para definir el presupuesto
@@ -15,8 +18,13 @@ const Pregunta = () => {
     e.preventDefault();
 
     //Validar
+    if(cantidad < 1 || isNaN(cantidad)){
+      guardarError(true);
+      return;
+    }
 
     //En caso de que pase la validación
+    guardarError(false);
   }
 
 
@@ -37,6 +45,7 @@ const Pregunta = () => {
           type="submit"
           className="button-primary u-full-width"
           value="Definir presupuesto"
+          onClick={agregarPresupuesto}
         />
       </form>
     </Fragment>
